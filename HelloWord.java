128ea78bfbf31e24d13c261685a2b43df10cb9a7
peoplefish <dingjuru@gmail.com>
2014-05-30 11:38:36 +0800

    This is a java language file

---
+++ HelloWord.java
@@ -0,0 +1,15 @@
+/**
+ * This is a HelloWord.java file
+ * @author 丁巨儒 <dingjuru@gmail.com>
+ * @time 2014 5-30
+ */
+public class HelloWord {
+
+	public HelloWord() {
+		System.out.println("Hello Word!");
+	}
+
+	public static void main(String[] args) {
+		new HelloWord();
+	}
+}
