08f76493d06b0feec1e0acb15f8aad45c632eb91
peoplefish <dingjuru@gmail.com>
2014-05-30 11:48:42 +0800

    This is a cpp file

---
+++ HelloWord.cpp
@@ -0,0 +1,13 @@
+/**
+ * this is a Hellowrod.cpp file
+ * @author 丁巨儒 <dingjuru@gmail.con>
+ * @time 2014-5-30
+ */
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello Word" << std:: endl;
+
+	return 0;
+}
