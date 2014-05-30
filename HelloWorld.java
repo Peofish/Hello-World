33f609342938468918e278856453010d55a59d9e
peoplefish <dingjuru@gmail.com>
2014-05-30 13:03:24 +0800

    Update and rename HelloWord.java to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,12 @@
 /**
- * This is a HelloWord.java file
+ * This is a HelloWorld.java file
  * @author 丁巨儒 <dingjuru@gmail.com>
- * @time 2014 5-30
+ * @time 2014-5-30
  */
 public class HelloWord {
 
 	public HelloWord() {
-		System.out.println("Hello Word!");
+		System.out.println("Hello World!");
 	}
 
 	public static void main(String[] args) {
